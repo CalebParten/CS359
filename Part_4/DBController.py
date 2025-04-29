1b60d476e00feebb301d2b804f349c583de3cd34
davidschmith123 <davidschmith123@gmail.com>
2025-04-29 17:41:15 -0600

    started part 4, i think userconnection method should lead to main menu method

---
+++ Part_4/DBController.py
@@ -0,0 +1,60 @@
+import sqlite3
+from sqlite3 import Error
+import sys
+import os
+
+class DBController:
+
+    # constructor that will create a controller
+    def __init__(self, database_name):
+        self.conn = None
+        self.db_name = database_name
+        db_exists = os.path.exists(self.db_name)
+        self.create_connection(self.db_name)
+
+        if not db_exists:
+            self.create_database()
+            self.insert_data()
+        
+        self.close_connection()
+        
+    #creates a connection to the database with the supplied name
+    def create_connection(self, db_file):
+        try:
+            conn = sqlite3.connect(db_file)
+            print("[INFO] Connection established: " + sqlite3.sqlite_version)
+
+        except Error as e:
+            print(e)
+
+        self.conn = conn
+
+    # uses the crtdb.sql file to create tables
+    def create_database(self):
+        raw_crtdb = open("crtdb.sql","r")
+        script_crtdb = raw_crtdb.read()
+
+        try:
+            curs = self.conn.cursor()
+            curs.executescript(script_crtdb)
+            print("Tables Created")
+        except Error as e:
+            print(e)
+        return
+    
+    # inserts data according to insdb.sql
+    def insert_data(self):
+        raw_insdb = open("insdb.sql")
+        script_insdb = raw_insdb.read()
+
+        try:
+            curs = self.conn.cursor()
+            curs.executescript(script_insdb)
+            print("Data Inserted into Tables")
+        except Error as e:
+            print(e)
+    
+    # closes the connection to the database
+    def close_connection(self):
+        self.conn.close()
+        print("[INFO] Connection Terminated: " + sqlite3.sqlite_version)
---
+++ Part_4/crtdb.sql
@@ -0,0 +1,71 @@
+CREATE TABLE Member (
+    memberId INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    phone VARCHAR(15),
+    address VARCHAR(100),
+    age INTEGER CHECK (age >= 15),
+    membershipStartDate DATE NOT NULL,
+    membershipEndDate DATE NOT NULL CHECK(membershipEndDate > membershipStartDate)
+);
+
+CREATE TABLE Class (
+    classId INTEGER PRIMARY KEY AUTOINCREMENT,
+    className VARCHAR(50),
+    classType VARCHAR(20) CHECK(classType IN ('Yoga', 'Zumba', 'HIIT', 'Weights')),
+    duration INTEGER CHECK (duration > 0),
+    classCapacity INTEGER CHECK(classCapacity > 0),
+    instructorId INTEGER,
+    gymId INTEGER,
+    FOREIGN KEY (instructorId) REFERENCES Instructor(instructorId),
+    FOREIGN KEY (gymId) REFERENCES GymFacility(gymId)
+);
+
+CREATE TABLE Instructor (
+    instructorId INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) NOT NULL,
+    specialty VARCHAR(50),
+    phone VARCHAR(15),
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE GymFacility (
+    gymId INTEGER PRIMARY KEY AUTOINCREMENT,
+    location varchar(100),
+    phone varchar(30),
+    manager varchar(50)
+);
+
+CREATE TABLE Equipment (
+    equipmentId INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR(30) CHECK(type = 'Cardio' OR type = 'Strength' OR type = 'Flexibility' OR type = 'Recovery'),
+    quantity INTEGER CHECK (quantity >= 0),
+    gymId INTEGER,
+    FOREIGN KEY (gymId) REFERENCES GymFacility(gymId)
+);
+
+CREATE TABLE MembershipPlan (
+    planId INTEGER PRIMARY KEY AUTOINCREMENT,
+    planType VARCHAR(20) CHECK(planType IN('Monthly', 'Annual')),
+    cost NUMERIC CHECK (cost >= 0)
+);
+
+CREATE TABLE Payment (
+    paymentId INTEGER PRIMARY KEY AUTOINCREMENT,
+    memberId INTEGER,
+    planId INTEGER,
+    amountPaid REAL NOT NULL CHECK(amountPaid >= 0),
+    paymentDate DATE NOT NULL,
+    FOREIGN KEY (memberId) REFERENCES Member(memberId),
+    FOREIGN KEY (planId) REFERENCES MembershipPlan(planId)
+);
+
+CREATE TABLE Attends (
+    memberId INTEGER,
+    classId INTEGER,
+    attendanceDate DATE NOT NULL,
+    PRIMARY KEY (memberId, classId, attendanceDate),
+    FOREIGN KEY (memberId) REFERENCES Member(memberId),
+    FOREIGN KEY (classId) REFERENCES Class(classId)
+);
